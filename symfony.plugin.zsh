572a977f8c328b5e638183447bc549d359efe6eb
Tomasz Wójcik <tomasz.prgtw.wojcik@gmail.com>
2017-03-31 02:22:10 +0200

    Create symfony.plugin.zsh

---
+++ symfony.plugin.zsh
@@ -0,0 +1,63 @@
+# Basic command completion
+
+_symfony_console_script() {
+    dir="$PWD";
+
+    # Upward search
+    while ((1)); do
+
+        if [[ -f "$dir/bin/console" ]]; then
+            # Symfony 3
+            echo "$dir/bin/console";
+            return 0;
+        elif [[ -f "$dir/app/console" ]]; then
+            # Symfony 2
+            echo "$dir/app/console";
+            return 0;
+        fi
+
+        [[ "$dir" == '' ]] && break;
+
+        dir="${dir%/*}";
+    done
+
+    return 1;
+}
+
+_symfony_console () {
+  echo "php `_symfony_console_script`"
+}
+
+_symfony_xdebug_console () {
+  echo "php -dxdebug.remote_autostart=1 `_symfony_console_script`"
+}
+
+_symfony2_get_command_list () {
+   `_symfony_console` --no-ansi | sed "1,/Available commands/d" | awk '/^  ?[^ ]+ / { print $1 }'
+}
+
+_symfony2 () {
+   compadd `_symfony2_get_command_list`
+}
+
+compdef _symfony2 '`_symfony_console`'
+compdef _symfony2 'app/console'
+compdef _symfony2 'bin/console'
+compdef _symfony2 sf
+
+#Alias
+alias sf='`_symfony_console`'
+alias sfcl='sf cache:clear'
+alias sfsr='sf server:run -vvv'
+alias sfcw='sf cache:warmup'
+alias sfroute='sf debug:router'
+alias sfcontainer='sf debug:container'
+alias sfgb='sf generate:bundle'
+alias sfdev='sf --env=dev'
+alias sfprod='sf --env=prod'
+
+#Xdebug enabled aliases (useful for debugging purposes)
+alias sfx='`_symfony_xdebug_console`'
+alias sfxcl='sfx cache:clear'
+alias sfxcw='sfx cache:warmup'
+alias sfxdev='sfx --env=dev'
